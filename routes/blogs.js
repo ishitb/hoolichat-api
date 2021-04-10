974027562d24df189f89956e4aa3e18efcc55735
ishitb <ib370@snu.edu.in>
2021-04-10 20:52:38 +0530

    Added 4 temp blog routes for reference

--- routes/blogs.js
+++ routes/blogs.js
@@ -10,6 +10,52 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            console.log(result);
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send({ message: 'internal Server Error' });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    Blog.findById(id)
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send({ message: 'internal Server Error' });
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Blog.findByIdAndDelete(req.params.id)
+        .then((result) => res.send({ message: `${req.params.id} Deleted!` }))
+        .catch((err) => {
+            console.log(err);
+            res.send({ message: 'internal Server Error' });
+        });
+});
+
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.updateOne({ _id: id }, req.body, (err) => console.log(err))
+        .then((result) => res.send(result.n > 0))
+        .catch((err) => {
+            console.log(err);
+            res.send({ message: 'internal Server Error' });
+        });
+});
+
 router.get('/add', (req, res) => {
     const blog = new Blog({
         title: 'new blog',
