5dc19d7bd89a03804552af1cd815522362f7780a
ishitb <ib370@snu.edu.in>
2021-05-24 23:37:09 +0530

    patch: socket cors

--- sockets/index.js
+++ sockets/index.js
@@ -3,12 +3,18 @@ const socketio = require('socket.io');
 const messageMethods = require('../controllers/message');
 
 const socketConnection = async (server) => {
-    global.io = socketio(server);
+    global.io = socketio(server, {
+        cors: {
+            origin: '*',
+        },
+    });
 
     /**
      * @param {socketio.Socket} socket
      */
     io.on('connection', (socket) => {
+        socket.emit('message', 'Hello');
+
         socket.on('disconnect', (skt) => {
             global.io.emit('disconneced', { skt });
         });
