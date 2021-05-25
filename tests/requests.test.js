9b4eedd2da3aba1163ae16d688a3862a2292ee86
ishitb <ib370@snu.edu.in>
2021-05-25 17:40:04 +0530

    feature: replaced mocha with jest. Added basic requests test

---
+++ tests/requests.test.js
@@ -0,0 +1,59 @@
+global.describe('User Registration Test', () => {
+    global.it('Registered Successfully', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('User Login Test', () => {
+    global.it('Logged in Successfully', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Workspace POST Test', () => {
+    global.it('New Workspace added!', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Workspace GET Test', () => {
+    global.it('Workspace results', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Workspace PATCH Test', () => {
+    global.it('Workspace Updated', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Workspace DELETE Test', () => {
+    global.it('Workspace Deleted successfully', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Room POST Test', () => {
+    global.it('New Room added!', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Room GET Test', () => {
+    global.it('Room results', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Room PATCH Test', () => {
+    global.it('Room Updated', () => {
+        global.expect(true).toBe(true);
+    });
+});
+
+global.describe('Room DELETE Test', () => {
+    global.it('Room Deleted successfully', () => {
+        global.expect(true).toBe(true);
+    });
+});
